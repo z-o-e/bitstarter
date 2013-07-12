a4ef2ca8b26bdf2ae1e01aa69528248a010faed6
Zoe <zoe.go.ahead@gmail.com>
2013-07-12 04:41:37 +0000

    continued to debug web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs'); 
-    response.send(fs.readFileSync('index.html','utf-8');
+    response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
