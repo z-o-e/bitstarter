9a2f18c22ff07a49de0607fc362d60af80286032
Ubuntu <ubuntu@ip-10-151-43-72.ec2.internal>
2013-07-20 19:08:40 +0000

    fixed typo of index.html and updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var fs=require('fs');
+var htmlfile="index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require('fs'); 
-    response.send(fs.readFileSync('index.html').toString());
+    var html = fs.readFileSync(htmlfile).toString(); 
+    response.send(html);
 });
-
-app.use(express.static(__dirname + '/bootstrap'));
+   
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
